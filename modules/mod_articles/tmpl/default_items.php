1946233a93d7254dfc8c4e781e581538e8d11bbb
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2024-08-24 16:20:44 +0200

    Get rif of bootstrap dependencies

--- modules/mod_articles/tmpl/default_items.php
+++ modules/mod_articles/tmpl/default_items.php
@@ -25,10 +25,10 @@ if ($params->get('articles_layout') == 1) {
             $displayInfo = $item->displayHits || $item->displayAuthorName || $item->displayCategoryTitle || $item->displayDate;
         ?>
         <li>
-            <article class="mod-articles-item <?php echo ($params->get('card_link') ? 'mod-articles-item-card card' : ''); ?>" itemscope itemtype="https://schema.org/Article">
+            <article class="mod-articles-item" itemscope itemtype="https://schema.org/Article">
 
                 <?php if ($params->get('item_title') || $displayInfo || $params->get('show_tags') || $params->get('show_introtext') || $params->get('show_readmore')) : ?>
-                    <div class="mod-articles-item-content <?php echo ($params->get('card_link') ? 'card-body' : ''); ?>">
+                    <div class="mod-articles-item-content">
 
                         <?php if ($params->get('item_title')) : ?>
                             <?php $item_heading = $params->get('item_heading', 'h4'); ?>
@@ -63,7 +63,7 @@ if ($params->get('articles_layout') == 1) {
                                 <?php if ($item->displayCategoryTitle) : ?>
                                     <dd class="mod-articles-category <?php echo ($params->get('info_layout') == 1 ? 'list-inline-item' : ''); ?>">
                                         <?php echo LayoutHelper::render('joomla.icon.iconclass', ['icon' => 'icon-folder-open icon-fw']); ?>
-                                        <?php if ($item->displayCategoryLink && $params->get('card_link') == 0) : ?>
+                                        <?php if ($item->displayCategoryLink) : ?>
                                             <a href="<?php echo $item->displayCategoryLink; ?>">
                                                 <?php echo $item->displayCategoryTitle; ?>
                                             </a>
@@ -103,7 +103,7 @@ if ($params->get('articles_layout') == 1) {
                         <?php if ($params->get('show_readmore')) : ?>
                             <p class="mod-articles-readmore">
 
-                                <<?php echo ($params->get('card_link') ? 'span' : 'a'); ?> class="btn btn-secondary" href="<?php echo $item->link; ?>">
+                                <a class="btn btn-secondary" href="<?php echo $item->link; ?>">
                                     <?php if ($item->params->get('access-view') == false) : ?>
                                         <?php echo Text::_('MOD_ARTICLES_REGISTER_TO_READ_MORE'); ?>
                                     <?php elseif ($item->alternative_readmore) : ?>
@@ -118,7 +118,7 @@ if ($params->get('articles_layout') == 1) {
                                     <?php else : ?>
                                         <?php echo Text::_('MOD_ARTICLES_READ_MORE_TITLE'); ?>
                                     <?php endif; ?>
-                                </<?php echo ($params->get('card_link') ? 'span' : 'a'); ?>>
+                                </a>
                             </p>
                         <?php endif; ?>
                     </div>
